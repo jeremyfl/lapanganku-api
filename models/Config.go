845814f4c07dc563ca5e3f8c6b44b6a7e93b5d30
Jeremy Lombogia <jeremylombogia7@gmail.com>
2019-08-09 01:37:18 +0700

    fix: Fix up naming

--- models/Config.go
+++
@@ -1,5 +0,0 @@
-package models
-
-import "github.com/jeremylombogia/lapanganku-api/config"
-
-var db = config.Database()
--- partner/request.go
+++
@@ -1,8 +0,0 @@
-package partner
-
-type Payload struct {
-	Data struct {
-		VenueID uint   `json:"venueId"`
-		User    string `json:"user"`
-	} `json:"data"`
-}
